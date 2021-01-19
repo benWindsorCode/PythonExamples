66a7c938270c651844a1dda41ef73e245ed6efd2
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2021-01-19 08:44:05 +0000

    Linear regression code

--- code/LinearRegression.py
+++ code/LinearRegression.py
@@ -3,13 +3,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.api as sm
 
+# Start with data in DataFrame
 # y = 2x + 3
-points = 30
+points = 100
 x = list(range(points))
 y = [ 2*i + 3 for i in x ]
 
 d = { 'x' : x, 'y' : y }
 data = pd.DataFrame(data=d)
 
-data.plot()
+# Split out data for statsmodels
+exog = sm.add_constant(data.x)
+endog = data.y
+
+model = sm.OLS(endog, exog)
+fit = model.fit()
+
+print(fit.summary())
+
+data.plot(y='y')
 plt.show()
